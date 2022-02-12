78ea261adddcc63d2a91e73793e74113ae7c352d
재연 <charmant1402@gmail.com>
2022-02-12 18:35:27 +0900

    api

--- src/shared/api.js
+++ src/shared/api.js
@@ -6,4 +6,38 @@ const api = axios.create({
 
 api.defaults.headers.common["Authorization"] = "success";
 
-export const apis = {};
+const TEST_URL = (site) => `http://localhost:3003/${site}`;
+
+export const apis = {
+  signup: (id, email, pwd, nickname) =>
+    // api.post("/user/signup", {
+    api.post(TEST_URL("signup"), {
+      login_id: id,
+      email: email,
+      password: pwd,
+      nickname: nickname,
+    }),
+
+  login: (id, pwd) =>
+    api.post(TEST_URL("login"), {
+      login_id: id,
+      password: pwd,
+    }),
+
+  createArticle: (uid, image, tags, date) =>
+    api.post(TEST_URL("pictures"), {
+      uid: uid,
+      image_url: image,
+      tags: [...tags],
+      created_date: date,
+    }),
+
+  loginCheck: (id) =>
+    api.get(TEST_URL("idCheck")).then((res) => {
+      console.log(res);
+    }),
+
+  // logOut: () => api.get(TEST_URL("logout")),
+
+  // .catch(console.log("no")),
+};
