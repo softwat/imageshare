d05c1efffdb19625583a84e03c621a397ee2673c
softwat <ghr95223@gmail.com>
2022-02-16 17:09:18 +0900

    like

--- src/components/ImgInfo.js
+++ src/components/ImgInfo.js
@@ -1,42 +1,42 @@
-import React from "react";
-import styled from "styled-components";
-import { TagList } from "./index";
+import React from 'react';
+import styled from 'styled-components';
+import { TagList } from './index';
 
-const ImgInfo = (props) => {
-  const {
-    article_id,
-    created_date,
-    image_url,
-    isLike,
-    liked_users,
-    nickname,
-    tags,
-    uid,
-    writer_id,
-    writer_nickname,
-  } = props;
-  return (
-    <React.Fragment>
-      <ImgInfoContainer>
-        <p>좋아요 {liked_users?.length} 개</p>
-        <TagList tags={tags} />
-      </ImgInfoContainer>
-    </React.Fragment>
-  );
+const ImgInfo = props => {
+    const {
+        article_id,
+        created_date,
+        image_url,
+        isLike,
+        liked_users,
+        nickname,
+        tags,
+        uid,
+        writer_id,
+        writer_nickname,
+    } = props;
+    return (
+        <React.Fragment>
+            <ImgInfoContainer>
+                <p>좋아요 {liked_users?.length ? liked_users?.length : 0} 개</p>
+                <TagList tags={tags} />
+            </ImgInfoContainer>
+        </React.Fragment>
+    );
 };
 
 export default ImgInfo;
 
 const ImgInfoContainer = styled.div`
-  /* display: flex;
+    /* display: flex;
   align-items: start;
   justify-content: space-between; */
-  position: absolute;
-  width: 100%;
-  min-height: 30%;
-  bottom: 0;
-  padding: 20px;
-  color: #fff;
-  opacity: 0;
-  transition: all 0.3s;
+    position: absolute;
+    width: 100%;
+    min-height: 30%;
+    bottom: 0;
+    padding: 20px;
+    color: #fff;
+    opacity: 0;
+    transition: all 0.3s;
 `;
