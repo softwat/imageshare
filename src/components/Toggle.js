796bff2b83ed7b76bb2f90da40d1a1413a2d2dca
재연 <charmant1402@gmail.com>
2022-02-17 14:24:28 +0900

    로그인 회원가입 폼 스타일 수정

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,70 +1,70 @@
-import React from 'react';
-import styled from 'styled-components';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
 
-import { actionCreators as acticleActions } from '../redux/modules/article';
+import { actionCreators as acticleActions } from "../redux/modules/article";
 
-const Toggle = props => {
-    const { history } = props;
-    const dispatch = useDispatch();
-    const mineClick = () => {
-        history.push('/myprofile/mylist');
-        dispatch(acticleActions.getMyArticleAPI());
-    };
-    const likeClick = () => {
-        history.push('/myprofile/mylike');
-        dispatch(acticleActions.getMyLikeAPI());
-    };
-    return (
-        <RadioToolBar>
-            <input
-                type="radio"
-                id="radioMine"
-                name="radioProfile"
-                value="mine"
-                onClick={mineClick}
-                // defaultChecked
-            />
-            <label htmlFor="radioMine">내가 올린 사진</label>
+const Toggle = (props) => {
+  const { history } = props;
+  const dispatch = useDispatch();
+  const mineClick = () => {
+    history.push("/myprofile/mylist");
+    dispatch(acticleActions.getMyArticleAPI());
+  };
+  const likeClick = () => {
+    history.push("/myprofile/mylike");
+    dispatch(acticleActions.getMyLikeAPI());
+  };
+  return (
+    <RadioToolBar>
+      <input
+        type="radio"
+        id="radioMine"
+        name="radioProfile"
+        value="mine"
+        onClick={mineClick}
+        // defaultChecked
+      />
+      <label htmlFor="radioMine">내가 올린 사진</label>
 
-            <input
-                type="radio"
-                id="radioLiked"
-                name="radioProfile"
-                value="liked"
-                onClick={likeClick}
-            />
-            <label htmlFor="radioLiked">내가 좋아요 한 사진</label>
-        </RadioToolBar>
-    );
+      <input
+        type="radio"
+        id="radioLiked"
+        name="radioProfile"
+        value="liked"
+        onClick={likeClick}
+      />
+      <label htmlFor="radioLiked">내가 좋아요 한 사진</label>
+    </RadioToolBar>
+  );
 };
 
 const RadioToolBar = styled.div`
-    margin-top: 30px;
-    display: flex;
-    justify-content: space-evenly;
-    input[type='radio'] {
-        opacity: 0;
-        position: fixed;
-        width: 0;
-    }
-    label {
-        width: 30%;
-        text-align: center;
-        display: inline-block;
-        background-color: #ddd;
-        padding: 10px 20px;
-        font-family: sans-serif, Arial;
-        font-size: 16px;
-        border-radius: 12px;
-        color: #fff;
-    }
-    input[type='radio']:checked + label {
-        background-color: red;
-    }
-    label:hover {
-        background-color: pink;
-    }
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-evenly;
+  input[type="radio"] {
+    opacity: 0;
+    position: fixed;
+    width: 0;
+  }
+  label {
+    width: 30%;
+    text-align: center;
+    display: inline-block;
+    background-color: #ddd;
+    padding: 10px 20px;
+    font-family: sans-serif, Arial;
+    font-size: 16px;
+    border-radius: 12px;
+    color: #fff;
+  }
+  input[type="radio"]:checked + label {
+    background-color: #ff54b0;
+  }
+  label:hover {
+    background-color: #ff54b0;
+  }
 `;
 
 export default Toggle;
